0b19de7dfd0a0cfe9afb033d67f0f3571f31c39b
Thomanphan <phannhatnghi@gmail.com>
2014-10-01 17:24:40 +0700

    Update demo_json.php

--- demo_json.php
+++ demo_json.php
@@ -12,4 +12,12 @@ curl_setopt($ch, CURLOPT_NOBODY, 0);
 
 $response  = curl_exec($ch);
 echo $response;
+## Display file status
+$result= json_decode($response);
+echo "Files status : ".$result->status;
+echo "Files name : ".$result->filename;
+echo "Files name : ".$result->filesize_mb." MB";
+## Check API used
+echo "API queried to day: ".$result->api_query ;
+echo "API limited :".$result->daily_limited;
 ?>
