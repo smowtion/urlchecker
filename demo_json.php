8a23f0f205b5859527e2e4b14ba7291c882173d1
Thomanphan <phannhatnghi@gmail.com>
2014-11-06 22:04:43 +0700

    add key

--- demo_json.php
+++ demo_json.php
@@ -1,11 +1,12 @@
 <?php
 $response_format="json";
+$key='demo';
 $link="http://www.mediafire.com/?wfghxg5x7d159x8";
 $api = 'http://api.urlchecker.net/';
 $ch = curl_init($api);
 
 curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, "response_format=$response_format&link=$link");
+curl_setopt($ch, CURLOPT_POSTFIELDS, "response_format=$response_format&key=$key&link=$link");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_VERBOSE, 1);
 curl_setopt($ch, CURLOPT_NOBODY, 0);
